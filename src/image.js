62bf369a86553f198ef53311ed3a893384be1fab
CryptosByte <>
2021-10-10 13:24:56 +0100

    Initial

---
+++ src/image.js
@@ -0,0 +1,67 @@
+const sharp = require("sharp");
+const axios = require("axios");
+
+module.exports = async (data) => {
+  try {
+    await sharp(
+      (
+        await axios({
+          url: data.url,
+          responseType: "arraybuffer",
+        })
+      ).data
+    )
+      .composite([
+        {
+          input: Buffer.from(
+            '<svg><rect x="0" y="0" width="48" height="48" rx="12" ry="12"/></svg>'
+          ),
+          blend: "dest-in",
+        },
+      ])
+      .png()
+      .toFile(`./tmp/${data.data}-1.png`);
+
+    await sharp("./imgs/bg.png")
+      .composite([
+        {
+          input: `./tmp/${data.data}-1.png`,
+          top: 15,
+          left: 30,
+        },
+        {
+          input: `./imgs/${data.presence}.png`,
+          top: 50,
+          left: 66,
+        },
+        {
+          input: Buffer.from(`
+          <svg width="390" height="60">
+            <style>
+            .title { fill: #fff; font-size: 30px; font-weight: bold; font-family: helvetica; }
+            </style>
+            <text x="40%" y="50%" text-anchor="" class="title">${data.username}</text>
+          </svg>
+          `),
+          top: 10,
+          left: -60,
+        },
+        {
+          input: Buffer.from(`
+          <svg width="394" height="60">
+            <style>
+            .title { fill: #fff; font-size: 25px; font-weight: bold; font-family: helvetica; }
+            </style>
+            <text x="82.5%" y="86%" text-anchor="" class="title">${data.discriminator}</text>
+          </svg>
+          `),
+          top: 10,
+          left: -60,
+        },
+      ])
+      .toFormat("png")
+      .toFile(`./tmp/${data.data}.png`);
+  } catch (e) {
+    console.error(e);
+  }
+};
---
+++ src/index.js
@@ -0,0 +1,91 @@
+const { Client, Intents } = require("discord.js"),
+  app = require("express")(),
+  ImageResponse = require("./image"),
+  exec = require("child_process").exec,
+  fs = require("fs");
+
+class MainClient {
+  constructor() {
+    // ********************
+    require("dotenv").config();
+    // ********************
+
+    this.client = new Client({
+      intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MEMBERS,
+        Intents.FLAGS.GUILD_PRESENCES,
+      ],
+    });
+
+    app.get("/", (request, response) => {
+      response.json({
+        clientPing: `${this.client.ws.ping}`,
+        server: "https://discord.gg/TUAkRBWwMZ",
+      });
+    });
+
+    app.get("/user_data/:id", async (request, response) => {
+      if (request.params.id && parseInt(request.params.id)) {
+        try {
+          const member = await this.client.guilds.cache
+            .get(process.env.SERVERID)
+            .members.fetch(request.params.id);
+
+          await ImageResponse({
+            url: `https://cdn.discordapp.com/avatars/${member.user.id}/${member.user.avatar}.png?size=48`,
+            data: member.user.id,
+            username: member.user.username,
+            discriminator: `#${member.user.discriminator}`,
+            presence: member.presence?.status || "offline",
+          });
+
+          var stream = fs.createReadStream(`./tmp/${request.params.id}.png`);
+
+          stream.on("open", async () => {
+            response.set("Content-Type", "image/png");
+            stream.pipe(response);
+
+            await exec("rm -rf tmp/*");
+          });
+        } catch {
+          return response.json({
+            success: false,
+            message: "Invalid 'userid' or user did not join server!",
+          });
+        }
+      } else
+        response.json({
+          success: false,
+          message: "'userid' is not a parse-able integer!",
+        });
+    });
+
+    this.client.on("ready", () =>
+      console.log(`[${this.client.user.username}] I'm now initialised.`)
+    );
+
+    this.client.on("rateLimit", () =>
+      console.log(`[${this.client.user.username}] I'm being RateLimited!`)
+    );
+  }
+
+  init = () => {
+    this.client.login(process.env.TOKEN);
+    app.listen(process.env.PORT || 8080, () =>
+      console.log("[Server] Dancing on Port %s", process.env.PORT || 8080)
+    );
+  };
+}
+
+new MainClient().init();
+
+/*
+return {
+  username: member.user.username,
+  discriminator: member.user.discriminator,
+  avatarURL: `https://cdn.discordapp.com/avatars/${member.user.username}/${member.user.avatar}.png`,
+  status: member.presence?.status || "offline",
+  activities: member.presence?.activities || [],
+};
+*/
